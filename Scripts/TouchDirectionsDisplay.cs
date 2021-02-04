20e4e5d321f0304d4457dd2b77a0813ac7661fef
Eren Inanci <erencan_inanci@hotmail.com>
2021-02-04 18:52:59 +0300

    mobileInputRightWay

---
+++ Scripts/TouchDirectionsDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchDirectionsDisplay : MonoBehaviour
+{
+    [SerializeField] private Text _directionDisplayText;
+    private Touch _theTouch;
+    private Vector2 _touchStartPos, _touchEndPos;
+    private string _direction;
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            _theTouch = Input.GetTouch(0);
+            if (_theTouch.phase == TouchPhase.Began)
+            {
+                _touchStartPos = _theTouch.position;
+            }
+            else if (_theTouch.phase == TouchPhase.Moved || _theTouch.phase == TouchPhase.Ended)
+            {
+                _touchEndPos = _theTouch.position;
+                float x = _touchEndPos.x - _touchStartPos.x;
+                float y = _touchEndPos.y - _touchStartPos.y;
+
+                if (Mathf.Abs(x) == 0 && Mathf.Abs(y) == 0) //mutlak deger
+                {
+                    _direction = "Tapped";
+                }
+                else if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    _direction = x > 0 ? "Right" : "Left";
+                }
+                else if (Mathf.Abs(y) > Mathf.Abs(x))
+                {
+                    _direction = y > 0 ? "Up" : "Down";
+                }
+            }
+        }
+        _directionDisplayText.text = _direction;
+    }
+}
---
+++ Scripts/TouchPhaseDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchPhaseDisplay : MonoBehaviour
+{
+    [SerializeField] private Text _phaseDisplayText;
+    private Touch _theTouch;
+    private float timeTouchEnded;
+    private float displayTime = 0.25f;
+    void Update()
+    {
+        if(Input.touchCount > 0) 
+        {
+            _theTouch = Input.GetTouch(0);
+            if (_theTouch.phase == TouchPhase.Ended)
+            {
+                _phaseDisplayText.text = _theTouch.phase.ToString();
+                timeTouchEnded = Time.time;
+            }
+            else if (Time.time - timeTouchEnded > displayTime)
+            {
+                _phaseDisplayText.text = _theTouch.phase.ToString();
+                timeTouchEnded = Time.time;
+            }
+        }
+        else if (Time.time - timeTouchEnded > displayTime)
+        {
+            _phaseDisplayText.text = "";
+        }
+    }
+}
